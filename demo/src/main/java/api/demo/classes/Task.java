6cd3acc0077a1741926ca691f0c73c5dc0a79b34
JhonatanLop <jhooliveira.lopes@gmail.com>
2023-11-15 00:09:17 -0300

    adicionando dependências e classe principal

--- demo/src/main/java/api/demo/classes/Task.java
+++ demo/src/main/java/api/demo/classes/Task.java
@@ -1,5 +1,30 @@
 package api.demo.classes;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "task")
+@Table(name = "task")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
-    
+    @Column(name = "tsk_id")
+    private long id;
+    @Column(name = "tsk_name")
+    private String name;
+    @Column(name = "tsk_description")
+    private String description;
+    @Column(name = "tsk_due_date")
+    private Date dueDate;
+    @Column(name = "tsk_completed")
+    private boolean completed;
 }
